d54943af834e5b051ac92af1146ed82e8297e532
Ethan721au <158656876+Ethan721au@users.noreply.github.com>
2024-07-03 20:59:47 +1000

    Update builder.tsx

--- components/InfiniteScroll/builder.tsx
+++ components/InfiniteScroll/builder.tsx
@@ -1,1 +1,15 @@
-er
+Builder.registerComponent(withChildren(InfiniteScroll), {
+	name: "InfiniteScroll",
+	inputs: [
+		{
+			name: "speed",
+			type: "string",
+			defaultValue: "normal",
+			enum: ["slow", "normal", "fast"]
+		},
+		{ name: "itemPadding", type: "number", defaultValue: 10 },
+		{ name: "active", type: "boolean", defaultValue: true },
+		{ name: "orientation", type: "enum", defaultValue: "horizontal", enum: ["horizontal", "vertical"] },
+		{ name: "direction", type: "enum", defaultValue: "normal", enum: ["normal", "reverse"] }
+	]
+});
