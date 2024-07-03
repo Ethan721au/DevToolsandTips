a08eb257d054c0676c1568cd16c83d7f5c01629c
Ethan721au <158656876+Ethan721au@users.noreply.github.com>
2024-07-03 21:16:00 +1000

    Update builder.tsx

--- components/Carousel/builder.tsx
+++ components/Carousel/builder.tsx
@@ -1,1 +1,12 @@
-df
+Builder.registerComponent(withChildren(CarouselCMS), {
+	name: "Carousel",
+	inputs: [
+		{
+			name: "padding",
+			type: "number",
+			defaultValue: "20"
+		},
+		{ name: "width", type: "number", defaultValue: "150" },
+		{ name: "accentColor", type: "colour" }
+	]
+});
